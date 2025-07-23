76ce24de4cb22ec2d3e01778009d1c067f1059ac
Max Savio <fmaxsavio@gmail.com>
2025-07-23 14:02:41 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+const questions = [
+  {
+    q: "The types of source given the the circuit are?",
+    options: ["AC and DC voltage", "DC voltage and current", "AC voltage and current", "Only DC source"],
+    answer: 1,
+  },
+  {
+    q: "The number of split circuits are?",
+    options: ["None", "1", "2", "3"],
+    answer: 2,
+  },
+  {
+    q: "In this problem current in branches can be easily found using?",
+    options: ["Ohm's law", "Kirchhoff's law", "Current division rule", "voltage division rule"],
+    answer: 2,
+  },
+  {
+    q: "The resistance in the branch of (I1)' and (I2)' are?",
+    options: ["10,30", "30,10", "10,20", "20,10"],
+    answer: 0,
+  },
+  {
+    q: "The resistance of the brach of the current (I2)' is in",
+    options: ["Series", "isolated", "parallel", "open circuited"],
+    answer: 2,
+  },
+  {
+    q: "The resistance in the branch of (I2)'' and (I3)'' are?",
+    options: ["10.33, 20", "20,10.33", "33.33,20", "20,33.33"],
+    answer: 0,
+  },
+  {
+    q: "The value of (I2)' is",
+    options: ["1.4", "0.4", "0.2", "1.2"],
+    answer: 1,
+  },
+  {
+    q: "The value of (I2)'' is",
+    options: ["1.4", "2.4", "3.4", "4.4"],
+    answer: 1,
+  },
+  {
+    q: "The total current through the 10 Ohm resistor is?",
+    options: ["0.4", "0.6", "0.8", "1.0"],
+    answer: 2,
+  },
+  {
+    q: "The current (I2)' and (I2)'' are in",
+    options: ["Same direction", "Opposite direction", "Cannot be determined", "information missing"],
+    answer: 1,
+  },
+  {
+    q: "The direction of the current in the 10 Ohm resistance is in the direction of?",
+    options: ["current from 20 V source", "same as 4 A source", "cannot be determined", "information missing"],
+    answer: 1,
+  },
+  ]
